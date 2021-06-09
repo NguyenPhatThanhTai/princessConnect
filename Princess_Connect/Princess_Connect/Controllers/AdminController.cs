d35d0ac58c6d50873c92cb3a28eb0701bc75571e
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-06-09 22:10:27 +0700

    Update #3

---
+++ Princess_Connect/Princess_Connect/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Princess_Connect.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Trangchu()
+        {
+            return View();
+        }
+
+        public ActionResult ThemNhanVat()
+        {
+            return View();
+        }
+    }
+}
